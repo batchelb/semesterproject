3a22a675d6a02cbf9548b77807d9b019bd06176e
root <root@ip-172-31-30-163.us-west-2.compute.internal>
2017-12-02 22:18:01 +0000

    db changes

--- IS415/settings.py
+++ IS415/settings.py
@@ -76,11 +76,11 @@ WSGI_APPLICATION = 'IS415.wsgi.application'
 DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
+            'NAME': 'ebdb',
+            'USER': 'ebroot',
+            'PASSWORD': 'ISRocks',
+            'HOST': '52.26.31.217',
+            'PORT': '5432',
         }
 }
 
